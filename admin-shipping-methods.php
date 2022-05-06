7655f3baef0b98aeefd19422826103eff6005261
Joel Cormack <dgowellkomrak@gmail.com>
2022-05-06 11:33:06 -0500

    initial commit

---
+++ admin-shipping-methods.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+* Plugin Name: Admin Shipping Methods
+* Description: Adds table rate shipping methods to admin add order page.
+* Version: 0.1
+* Author: TapaCode
+* Author URI: http://www.tapacode.com
+*/
+
+?>
