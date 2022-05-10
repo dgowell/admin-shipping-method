1b2109283261e6667dbf1e0c7f630f65aa90b427
Joel Cormack <dgowellkomrak@gmail.com>
2022-05-10 18:24:18 -0500

    remove helper functions

--- admin-shipping-method.php
+++ admin-shipping-method.php
@@ -1,14 +1,4 @@
 <?php
-
-/**
-* @function Get All WooCommerce Shipping Rates
-* @how-to Get CustomizeWoo.com FREE
-* @author Rodolfo Melogli
-* @compatible WooCommerce 5
-* @donate $9 https://businessbloomer.com/bloomer-armada/
-*/
-
-
 /**
 * Plugin Name: Admin Shipping Methods
 * Description: Adds table rate shipping methods to admin add order page.
@@ -158,48 +148,5 @@ if (
     * hook to add the ajax callback
     */
     add_action( 'wp_ajax_add_order_shipping', 'add_order_shipping' );
-}
-
-    /**
-    * @function Get WooCommerce Shipping Zones
-    * @how-to Get CustomizeWoo.com FREE
-    * @author Rodolfo Melogli
-    * @compatible WooCommerce 5
-    * @donate $9 https://businessbloomer.com/bloomer-armada/
-    */
-
-    function bbloomer_get_all_shipping_zones() {
-        $data_store = WC_Data_Store::load( 'shipping-zone' );
-        $raw_zones = $data_store->get_zones();
-        foreach ( $raw_zones as $raw_zone ) {
-            $zones[] = new WC_Shipping_Zone( $raw_zone );
-        }
-        $zones[] = new WC_Shipping_Zone( 0 ); // ADD ZONE "0" MANUALLY
-        return $zones;
-    }
-
-    /**
-    * @function Get All WooCommerce Shipping Rates
-    * @how-to Get CustomizeWoo.com FREE
-    * @author Rodolfo Melogli
-    * @compatible WooCommerce 5
-    * @donate $9 https://businessbloomer.com/bloomer-armada/
-    */
-
-    function bbloomer_get_all_shipping_rates() {
-        foreach ( bbloomer_get_all_shipping_zones() as $zone ) {
-            $zone_shipping_methods = $zone->get_shipping_methods();
-            foreach ( $zone_shipping_methods as $index => $method ) {
-                $method_is_taxable = $method->is_taxable();
-                $method_is_enabled = $method->is_enabled();
-                $method_instance_id = $method->get_instance_id();
-                $method_title = $method->get_method_title(); // e.g. "Flat Rate"
-                $method_description = $method->get_method_description();
-                $method_user_title = $method->get_title(); // e.g. whatever you renamed "Flat Rate" into
-                $method_rate_id = $method->get_rate_id(); // e.g. "flat_rate:18"
-            }
-        //print_r( $zone_shipping_methods );
-        return $zone_shipping_methods;
-    }
 }
 ?>
