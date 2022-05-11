457a59f32b67501369b9e474323dcb631d4eea28
Joel Cormack <dgowellkomrak@gmail.com>
2022-05-11 10:32:14 -0500

    increase version number

--- admin-shipping-method.php
+++ admin-shipping-method.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Admin Shipping Methods
 * Description: Adds table rate shipping methods to admin add order page.
-* Version: 0.1
+* Version: 0.2
 * Author: TapaCode
 * Author URI: http://www.tapacode.com
 */
