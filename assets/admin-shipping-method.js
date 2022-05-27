cb3737562c49b96f124697a5c006728a7eaed469
Joel Cormack <dgowellkomrak@gmail.com>
2022-05-27 17:42:41 -0500

    Handle one shipping option

--- assets/admin-shipping-method.js
+++ assets/admin-shipping-method.js
@@ -69,18 +69,24 @@
 
     $('#shipping-options').on('click','#submit-shipping', function(e){
         e.preventDefault();
-
+        var shipping_id, shipping_price, shipping_name;
         //get the order_id from the button tag
         var order_id = $(this).data('order_id');
         var nonce = $(this).data('nonce');
         //send the data via ajax to the sever
         var options  = e.target.form.elements['table_rate'];
         const choice = parseInt(e.target.form.elements['table_rate'].value);
-        //convert to array from node list
-        options = Array.from(options);
-        const shipping_id = options[choice].id;
-        const shipping_price = options[choice].dataset.price;
-        const shipping_name = options[choice].dataset.display_name;
+        //convert to array from node list if more than 1
+        if (options.length > 1) {
+            options = Array.from(options);
+            shipping_id = options[choice].id;
+            shipping_price = options[choice].dataset.price;
+            shipping_name = options[choice].dataset.display_name;
+        } else {
+            shipping_id = options.id;
+            shipping_price = options.dataset.price;
+            shipping_name = options.dataset.display_name;
+        }
 
         //work out which shippping option is checked and return the values
         //if input is checked
